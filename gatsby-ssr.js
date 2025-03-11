1ef247fe5cae682e90aaefa1f22e00ef96e8df88
heeung <skdi6031@naver.com>
2025-03-11 16:48:16 +0900

    modify: 사이트 한국어로

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -5,3 +5,8 @@
  */
 
 // You can delete this file if you're not using it
+import React from "react";
+
+export const onRenderBody = ({ setHtmlAttributes }) => {
+  setHtmlAttributes({ lang: "ko" });
+};
